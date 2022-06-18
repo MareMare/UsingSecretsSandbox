33f969c44db3723194f6b871f2ed0ee1dfb56d8a
MareMare <mare.fujii@gmail.com>
2022-06-19 02:31:23 +0900

    Create Fixture.cs

---
+++ src/ConsoleApp.Test/Fixture.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApp.Test;
+
+public class Fixture
+{
+    private readonly IConfiguration _config;
+
+    public Fixture()
+    {
+        // e.g.
+        // secrets.json for UnitTest
+        // {
+        //    "username": "ゆーざTest",
+        //    "apikey": "きーTest"
+        // }
+        this._config = new ConfigurationBuilder()
+            .AddEnvironmentVariables()
+            .AddUserSecrets<UnitTest1>()
+            .Build();
+        this.SetEnvironmentVariablesFromUserSecrets();
+    }
+
+    public IConfiguration Config => this._config;
+
+    private void SetEnvironmentVariablesFromUserSecrets()
+    {
+        foreach (var kvp in this._config.GetChildren())
+        {
+            Environment.SetEnvironmentVariable(kvp.Key, kvp.Value);
+        }
+    }
+}
